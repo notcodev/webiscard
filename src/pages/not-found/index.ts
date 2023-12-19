069ca78a1fb92af3ffffab56d6ebe5fbf463f9f7
nothugofsea <slmv_e@icloud.com>
2023-12-19 09:58:02 +0300

    feat: added not found page

---
+++ src/pages/not-found/index.ts
@@ -0,0 +1,7 @@
+import { NotFoundPage } from './page'
+import { currentRoute } from './model'
+
+export const NotFoundRoute = {
+  view: NotFoundPage,
+  route: currentRoute,
+}
---
+++ src/pages/not-found/page.tsx
@@ -0,0 +1,20 @@
+import { Anchor, Title, WebiscardIcon } from '~/shared/ui'
+import { routes } from '~/shared/routing.ts'
+
+export const NotFoundPage = () => {
+  return (
+    <div className="h-screen w-full flex flex-col justify-center items-center px-8">
+      <WebiscardIcon className="absolute top-12" />
+      <Title className="text-center" order={1}>
+        Oops, page not found
+      </Title>
+      <p className="text-lg text-center text-muted-foreground mt-2">
+        Perhaps you entered the address <br />
+        incorrectly, or such a page no longer exists.
+      </p>
+      <Anchor className="text-lg mt-4" to={routes.home}>
+        Go to main page
+      </Anchor>
+    </div>
+  )
+}
